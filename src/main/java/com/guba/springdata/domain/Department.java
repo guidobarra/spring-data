592d8362e0811856b1da1137566e567b8ec36146
guido <guidobarrra18@gmail.com>
2021-08-26 18:37:18 -0300

    add domains

---
+++ src/main/java/com/guba/springdata/domain/Department.java
@@ -0,0 +1,33 @@
+package com.guba.springdata.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "department")
+@Setter
+@Getter
+@ToString(exclude = "careers")
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "department", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Career> careers;
+}
