c1c350be08539a92197a2a0d73aecf9139d67372
guido <guidobarrra18@gmail.com>
2021-08-26 18:36:55 -0300

    init project

---
+++ src/main/java/com/guba/springdata/PostgreSQLApp.java
@@ -0,0 +1,12 @@
+package com.guba.springdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostgreSQLApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PostgreSQLApp.class, args);
+    }
+}
