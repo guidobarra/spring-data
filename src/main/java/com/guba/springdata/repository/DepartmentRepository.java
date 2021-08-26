dc388d999c188aeab749f5468b03e2176b5d04a5
guido <guidobarrra18@gmail.com>
2021-08-26 18:37:32 -0300

    add repositories

---
+++ src/main/java/com/guba/springdata/repository/DepartmentRepository.java
@@ -0,0 +1,9 @@
+package com.guba.springdata.repository;
+
+import com.guba.springdata.domain.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+}
